a5247387adee8267c65074e916c2a39eae1e23a2
klina8575 <klina8575@gmail.com>
2024-01-12 00:23:11 +0900

    up

---
+++ 03.jQuery/chapter05.비동기(Ajax)/ajax2.js
@@ -0,0 +1,16 @@
+var openWeatherAPI = 'https://api.openweathermap.org/data/2.5/weather?appid=f98e648c60dee9415bd3b65e176b86ca&units=metric&lang=kr&q=seoul'
+
+$.ajax({
+   type: 'GET',
+   url: openWeatherAPI,
+   dataType: 'json',
+   success: function (data) {
+      console.log(data)
+   },
+   error: function (request, status, error) {
+      //응답 에러시 처리 작업
+      console.log('code:' + request.status)
+      console.log('message:' + request.responseText)
+      console.log('error:' + error)
+   },
+})
