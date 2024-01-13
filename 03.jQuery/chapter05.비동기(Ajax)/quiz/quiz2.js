85b1e5e4ccf911153d249ebe952aa9ac18bc25af
klina8575 <klina8575@gmail.com>
2024-01-13 12:14:43 +0900

    up

---
+++ 03.jQuery/chapter05.비동기(Ajax)/quiz/quiz2.js
@@ -0,0 +1,18 @@
+//1. 서울 날씨 작성
+var openWeatherAPI = 'https://api.openweathermap.org/data/2.5/weather?appid=f98e648c60dee9415bd3b65e176b86ca&units=metric&lang=kr&q=seoul'
+
+$.ajax({
+   type: 'GET',
+   url: openWeatherAPI,
+   dataType: 'json',
+   success: function (data) {
+      console.log(data)
+      //코드를 작성하세요
+   },
+   error: function (request, status, error) {
+      //응답 에러시 처리 작업
+      console.log('code:' + request.status)
+      console.log('message:' + request.responseText)
+      console.log('error:' + error)
+   },
+})
